d3d904167fd12f2dc0cba49c87e1bfd0f7e757af
AnneLineC <anneline.col@gmail.com>
2021-10-09 09:26:34 +0200

    HTML, CSS et JS du menu en place

---
+++ frontend/assets/js/app.js
@@ -0,0 +1,28 @@
+const app = {
+    init: function() {
+
+        // ----------------------------------------------------------------
+        // Menu burger responsive
+        // ----------------------------------------------------------------
+
+        // On cache le menu au lancement de la page grâce à JS, ainsi il restera accessible même si JS est désactivé.
+        // Bon, le reste du site ne marchera plus dans ce cas là, mais c'est pour la bonne habitude ;)
+        const nav = document.getElementById('main-nav');
+        nav.classList.add('hidden');
+
+        const burgerIcon = document.getElementById('burger');
+        burgerIcon.addEventListener('click', app.handleBurgerMenuClick);
+    },
+
+    /**
+     * Méthode permettant de gérer l'affichage du menu au clic sur le burger (en version mobile)
+     */
+     handleBurgerMenuClick: function() {
+        const nav = document.getElementById('main-nav');
+        nav.classList.toggle('hidden');
+    }
+}
+
+document.addEventListener( "DOMContentLoaded", app.init );
+
+
